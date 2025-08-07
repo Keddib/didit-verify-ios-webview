e1dde44a0cb756eb8a29a5f923a997f917312834
Khalid Eddib <khalid.eddib@gamiumworld.com>
2025-08-07 16:57:18 +0200

    Initial Commit

---
+++ didit-verify-webview/didit_verify_webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  didit_verify_webviewApp.swift
+//  didit-verify-webview
+//
+//  Created by keddib on 7/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct didit_verify_webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ didit-verify-webviewTests/didit_verify_webviewTests.swift
@@ -0,0 +1,16 @@
+//
+//  didit_verify_webviewTests.swift
+//  didit-verify-webviewTests
+//
+//  Created by keddib on 7/8/2025.
+//
+
+import Testing
+
+struct didit_verify_webviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
